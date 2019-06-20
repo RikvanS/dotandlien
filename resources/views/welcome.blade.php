ffb45f8fe5d0b67e4c6750142236d678f9301405
Rik <Rikvstraaten@gmail.com>
2019-06-20 23:19:31 +0200

    Social buttons, cleaning up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,19 @@
-@extends('partials.header')
+<link rel="stylesheet" type="text/css" href="{{ url('/css/main.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/css/heroheader.css') }}" />
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Barlow&display=swap" rel="stylesheet">
+
+<title>{{config('app.name')}}</title>
+
 <section class="hero">
     <div class="hero-inner">
-    <h1>Dot & Lien</h1>
-    <h2>Dotwork Illustrations</h2>
-    <a href="#contact-form" class="button">Contact me!</a>
+    <h1 class="violet">Dot & Lien</h1>
+    <h2 class="violet">Dotwork Illustrations</h2>
+    <a href="#contact-form" class="button violet">Contact me!</a>
     </div>
     </section>
+    <h1 class="titleheader">Dot&Lien</h1>
+<footer>
+    <h3>Find me on:</h3>
+    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/dot.and.lien/"><img src="../images/instagram.png" alt="Instagram" class="footer-link"></a>
+    <a target="_blank" rel="noopener noreferrer" href="https://nl.pinterest.com/dotandlien/my-work-+-illustrations/"><img src="../images/pinterest.png" alt="Pinterest" class="footer-link"></a>
+</footer>
